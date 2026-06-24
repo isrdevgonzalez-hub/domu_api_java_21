bc293664f48d9f248d0eec98f171c5c6ecfcaaef
isrdevgonzalez-hub <isr.dev.gonzalez@gmail.com>
2026-06-24 15:56:08 -0400

    inicio

---
+++ src/main/java/com/api/domu/domu/DomuApplication.java
@@ -0,0 +1,13 @@
+package com.api.domu.domu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/domu/domu/DomuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.domu.domu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
